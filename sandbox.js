b4e41c27fbc1bcfb90f8dd72d030f0a1b4dff531
SmolinPavel <isingwithaz@gmail.com>
2021-07-05 19:20:40 +0300

    Clean folders

--- sandbox.js
+++ sandbox.js
@@ -1,13 +1,1 @@
-// DOM navigation
-const ulEl = document.getElementById('example');
 
-const first = ulEl.firstElementChild;
-const second = first.nextElementSibling;
-const third = second.nextElementSibling;
-
-// console.log(first.firstElementChild.nextElementSibling);
-
-// Grapes
-const myDiv = document.createElement('div');
-myDiv.innerHTML = '<span>Hello</span>';
-second.appendChild(myDiv);
