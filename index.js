46cabad0cd91a0b4ac161a43abcb08ad34221b70
SmolinPavel <isingwithaz@gmail.com>
2021-07-30 16:35:04 +0300

    Add LS example

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('1')
+const refs = {
+  name: document.getElementById('name'),
+  message: document.getElementById('message'),
+  agreement: document.getElementById('agreement'),
+  age: document.getElementById('age'),
+};
+
+localStorage.setItem('name', 'Tatiana');
+
+window.addEventListener('load', () => {
+  const message = localStorage.getItem('message');
+  const agreement = localStorage.getItem('agreement');
+
+  refs.name.value = localStorage.getItem('name');
+  refs.message.value = message;
+  refs.agreement.checked = agreement === 'true';
+});
