fb86b8dd010c95ccbca458891b06e444c409329f
SmolinPavel <isingwithaz@gmail.com>
2021-08-11 19:47:27 +0300

    Add basic Pnotify example

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
+import { alert, defaultModules } from '@pnotify/core';
+import '@pnotify/core/dist/PNotify.css';
+import * as PNotifyMobile from '@pnotify/mobile';
+import '@pnotify/mobile/dist/PNotifyMobile.css';
+import '@pnotify/core/dist/BrightTheme.css';
+
+defaultModules.set(PNotifyMobile, {});
+
 const refs = {
   input: document.getElementById('input-id'),
   countriesList: document.getElementById('countries'),
 };
 
-refs.input.addEventListener('input', handleSearch);
+refs.input.addEventListener('input', handleInput);
 
-function handleSearch(e) {
+function handleInput(e) {
   const query = e.target.value;
   if (query) {
     fetch(`https://restcountries.eu/rest/v2/name/${query}`)
@@ -14,8 +22,13 @@ function handleSearch(e) {
       })
       .then((countries) => {
         const countriesHtml = countries
-          .map((country) => `<h1>${country.name}</h1>`)
+          .map((country) => `<h6>${country.name}</h6>`)
           .join('');
+
+        alert({
+          type: 'notice',
+          text: 'Hello JS #34!',
+        });
 
         refs.countriesList.insertAdjacentHTML('afterbegin', countriesHtml);
       })
